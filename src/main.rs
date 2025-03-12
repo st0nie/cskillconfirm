dd9c0bba3797d04d50251483cb07812287f3929f
ston <ston.jia@outlook.com>
2025-03-12 17:10:08 +0800

    fix bugs when switching to other players

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::sync::{
-    atomic::{AtomicU16, Ordering as ordering},
-    Arc,
+use std::{
+    fs::File,
+    io::BufReader,
+    sync::{Arc, Mutex},
+    thread,
 };
-use std::thread;
 
 use axum::{extract::State, routing::post, Json, Router};
+use clap::Parser;
 use gsi_cs2::Body;
 
-async fn update(State(kills): State<Arc<AtomicU16>>, data: Json<Body>) {
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// sound preset to use (available: "varolant")
+    #[arg(short, long, default_value = "varolant")]
+    preset: String,
+}
+
+struct AppState {
+    ply_name: String,
+    ply_kills: u16,
+    preset: String,
+}
+
+async fn update(State(app_state): State<Arc<Mutex<AppState>>>, data: Json<Body>) {
+    print!("\x1B[2J\x1B[1;1H"); //clear
     let map = data.map.as_ref();
     if let None = map {
-        println!("You need to load map");
         return;
     }
 
@@ -24,21 +38,25 @@ async fn update(State(kills): State<Arc<AtomicU16>>, data: Json<Body>) {
     let player = player_data.unwrap();
     let state = player.state.as_ref().unwrap();
 
+    let mut app_state = app_state.lock().unwrap();
+
     let current_kills = state.round_kills;
-    // let current_headshots = state.round_killhs;
-    let original_kills = kills.load(ordering::Relaxed);
+    let original_kills = app_state.ply_kills;
 
-    if current_kills != original_kills {
-        kills.store(current_kills, ordering::Relaxed);
-    }
-    print!("\x1B[2J\x1B[1;1H"); //clear
+    let current_name = player.name.as_ref().unwrap();
+    let original_name = &app_state.ply_name;
 
-    if current_kills == original_kills + 1 {
+    println!("Kills: {}", current_kills);
+    println!("Name: {}", current_name);
+
+    if current_kills == original_kills + 1 && (current_name == original_name || original_name == "")
+    {
         let sound_num = if current_kills > 5 { 5 } else { current_kills };
+        let preset = app_state.preset.to_string();
 
         thread::spawn(move || {
             let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-            let file = File::open(format!("sounds/varolant/{}.mp3", sound_num)).unwrap();
+            let file = File::open(format!("sounds/{}/{}.mp3", preset, sound_num)).unwrap();
             let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
 
             let sink = rodio::Sink::try_new(&stream_handle).unwrap();
@@ -49,18 +67,25 @@ async fn update(State(kills): State<Arc<AtomicU16>>, data: Json<Body>) {
         });
     }
 
-    println!("Kills: {}", current_kills);
+    app_state.ply_kills = current_kills;
+    app_state.ply_name = current_name.to_string();
 }
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+
     tracing_subscriber::fmt::init();
 
-    let kills = Arc::new(AtomicU16::new(0));
+    let app_state = Arc::new(Mutex::new(AppState {
+        ply_name: "".to_string(),
+        ply_kills: 0,
+        preset: args.preset,
+    }));
 
     let app = Router::new()
         .route("/", post(update))
-        .with_state(kills.clone());
+        .with_state(app_state.clone());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
