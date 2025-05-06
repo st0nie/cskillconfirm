e5700fa8cb3edd86948ec9bda989f306ddb5bc5d
ston <ston.jia@outlook.com>
2025-05-06 23:43:08 +0800

    refactor: use JoinSet

--- src/util/handler.rs
+++ src/util/handler.rs
@@ -4,7 +4,7 @@ use std::{fs::File, io::BufReader, sync::Arc};
 use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
 use gsi_cs2::Body;
 use thiserror::Error;
-use tokio::signal;
+use tokio::{signal, task::JoinSet};
 use tracing::{error, info, warn};
 
 use crate::AppState;
@@ -18,6 +18,18 @@ impl IntoResponse for ApiError {
     }
 }
 
+async fn add_file_to_mixer(
+    file_name: &str,
+    controller: &rodio::dynamic_mixer::DynamicMixerController<i16>,
+) -> Result<()> {
+    let file =
+        File::open(file_name).with_context(|| format!("failed to open file: {}", file_name))?;
+    let source = rodio::Decoder::new(BufReader::new(file))
+        .with_context(|| format!("failed to decode file: {:?}", file_name))?;
+    controller.add(source);
+    Ok(())
+}
+
 async fn play_audio(
     app_state_clone: Arc<AppState>,
     sound_num: u16,
@@ -29,70 +41,68 @@ async fn play_audio(
     let args = &app_state_clone.args;
     let preset = &app_state_clone.preset;
     let stream_handle = &app_state_clone.stream_handle;
-    let preset_name = args.preset.to_string();
+    let preset_name = Arc::new(args.preset.to_string());
     let volume = args.volume;
 
     let (controller, mixer) = rodio::dynamic_mixer::mixer::<i16>(2, 44100);
     let sink = rodio::Sink::try_new(stream_handle)?;
 
-    let add_file_to_mixer = async |file_name: &str| -> Result<()> {
-        let file =
-            File::open(file_name).with_context(|| format!("failed to open file: {}", file_name))?;
-        let source = rodio::Decoder::new(BufReader::new(file))
-            .with_context(|| format!("failed to decode file: {:?}", file_name))?;
-        controller.add(source);
-        anyhow::Ok(())
-    };
+    let mut tasks = JoinSet::new();
 
-    let play_common = async {
-        if preset.has_common_headshot && current_hs_kills > origin_hs_kills {
-            add_file_to_mixer(&format!("sounds/{}/common_headshot.wav", preset_name)).await?
-        } else if preset.has_common {
-            add_file_to_mixer(&format!("sounds/{}/common.wav", preset_name)).await?
-        }
-        anyhow::Ok(())
-    };
+    let preset_name_clone = preset_name.clone();
+    let controller_clone = controller.clone();
+    if preset.has_common_headshot && current_hs_kills > origin_hs_kills {
+        tasks.spawn(async move {
+            add_file_to_mixer(
+                &format!("sounds/{}/common_headshot.wav", preset_name_clone),
+                &controller_clone,
+            )
+            .await
+        });
+    } else if preset.has_common {
+        tasks.spawn(async move {
+            add_file_to_mixer(
+                &format!("sounds/{}/common.wav", preset_name_clone),
+                &controller_clone,
+            )
+            .await
+        });
+    }
 
-    let play_headshot = async {
-        if preset.has_headshot && !args.no_voice && current_hs_kills == 1 && current_kills == 1 {
-            let file_path = if preset.has_variant && args.variant.is_some() {
-                format!(
-                    "sounds/{}_v_{}/headshot.wav",
-                    preset_name,
-                    args.variant.as_ref().unwrap()
-                )
-            } else {
-                format!("sounds/{}/headshot.wav", preset_name)
-            };
-            add_file_to_mixer(&file_path).await?
-        }
-        anyhow::Ok(())
-    };
+    let controller_clone = controller.clone();
+    if preset.has_headshot && !args.no_voice && current_hs_kills == 1 && current_kills == 1 {
+        let file_path = if preset.has_variant && args.variant.is_some() {
+            format!(
+                "sounds/{}_v_{}/headshot.wav",
+                preset_name,
+                args.variant.as_ref().unwrap()
+            )
+        } else {
+            format!("sounds/{}/headshot.wav", preset_name)
+        };
+        tasks.spawn(async move { add_file_to_mixer(&file_path, &controller_clone).await });
+    }
 
-    let play_voice = async {
-        if preset.has_voice
-            && !args.no_voice
-            && (current_kills >= preset.start || !preset.has_headshot)
-            && current_kills <= sound_num_max
-            || !preset.has_common
-        {
-            let file_path = if preset.has_variant && args.variant.is_some() {
-                format!(
-                    "sounds/{}_v_{}/{}.wav",
-                    preset_name,
-                    args.variant.as_ref().unwrap(),
-                    sound_num
-                )
-            } else {
-                format!("sounds/{}/{}.wav", preset_name, sound_num)
-            };
-            add_file_to_mixer(&file_path).await?;
-        }
+    if preset.has_voice
+        && !args.no_voice
+        && (current_kills >= preset.start || !preset.has_headshot)
+        && current_kills <= sound_num_max
+        || !preset.has_common
+    {
+        let file_path = if preset.has_variant && args.variant.is_some() {
+            format!(
+                "sounds/{}_v_{}/{}.wav",
+                preset_name,
+                args.variant.as_ref().unwrap(),
+                sound_num
+            )
+        } else {
+            format!("sounds/{}/{}.wav", preset_name, sound_num)
+        };
+        tasks.spawn(async move { add_file_to_mixer(&file_path, &controller).await });
+    }
 
-        anyhow::Ok(())
-    };
-
-    let results = vec![play_common.await, play_headshot.await, play_voice.await];
+    let results = tasks.join_all().await;
 
     sink.append(mixer);
     sink.set_volume(volume);
@@ -100,10 +110,10 @@ async fn play_audio(
 
     results.iter().for_each(|result| {
         if let Err(e) = result {
-            error!("Error playing sound: {}", e);
+            error!("Failed to add file to mixer: {}", e);
         }
     });
-
+    
     sink.sleep_until_end();
     Ok(())
 }
